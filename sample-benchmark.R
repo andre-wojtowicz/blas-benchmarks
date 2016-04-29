c3480c7b84fa824b7cb1c475d6e85dca7de5cc84
Andrzej Wójtowicz <andre@amu.edu.pl>
2016-04-29 14:13:49 +0200

    added sample benchmark

---
+++ sample-benchmark.R
@@ -0,0 +1,16 @@
+library(checkpoint)
+library(rbenchmark)
+
+f = function()
+{
+set.seed (1)
+m <- 1000
+n <- 500
+A <- matrix (runif (m*n),m,n)
+
+# Matrix multiply
+B <- crossprod(A)
+}
+
+benchmark(f(), replications=100)
+
